ecf0f62d8484d9a599102f5c414c33ffd589cd7d
santosh <santosh.shinde@sixfactors.ca>
2017-07-10 11:21:10 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {ImageCropperComponent, CropperSettings, Bounds} from 'ng2-img-cropper';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageCropperComponent
   ],
   imports: [
     BrowserModule,
